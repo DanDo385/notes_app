ff552bbc6fe3ed2ed55753baeeb38f94d7fa3858
Daniel Magro <djm385@icloud.com>
2022-02-15 21:11:24 -0500

    starting js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const addBtn = document.getElementById('add')
 
+const notes = JSON.parse(localStorage.getItem('notes'))
+
+function addNewNote(text = '') {
+    const note = document.createElement('div')
+    note.classList.add('note')
